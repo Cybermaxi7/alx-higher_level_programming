f0e80ff9701e4990eb511d8a74450750b020a781
Maximus Agbe <maximusagbe@gmail.com>
2022-12-01 13:20:01 -0800

    update code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,7 +5,7 @@ and print sum result of two variables
 """
 
 if __name__ == "__main__":
-    from add-0 import add
+    from add_0 import add
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

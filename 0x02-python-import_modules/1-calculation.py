d42a9a6e0332594e9bc649713bddf9734f2610a5
Maximus Agbe <maximusagbe@gmail.com>
2022-12-01 13:40:08 -0800

    update pycodestyle standard

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,10 +5,10 @@ and perform operations on variables
 """
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, div, mul 
+    from calculator_1 import add, sub, div, mul
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b))) 
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))

4aac9cfdff91b2b9dee0a864b835f8a1455fab9c
Maximus Agbe <maximusagbe@gmail.com>
2022-12-01 13:19:00 -0800

    update code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""                                                           
-Simple program that import add function.                                        And print the sum result of two variables                                                                                          
-"""                                                                                                                                
+"""
+A program that import add function
+and print sum result of two variables
+"""
 
-if __name__ == "__main__":                                                      
-    from add_0 import add                                                           a = 1                                                       
-    b = 2                                                                       
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))  
+if __name__ == "__main__":
+    from add-0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

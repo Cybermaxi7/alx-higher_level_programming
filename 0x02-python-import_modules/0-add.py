18362920df8e8497f3db8b74d34b7dd84de7ce9e
Maximus Agbe <maximusagbe@gmail.com>
2022-12-01 04:30:20 -0800

    update code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,8 +4,8 @@ Simple program that import add function.
 And print the sum result of two variables
 """
 from add_0 import add
-a = 1
-b = 2
 
 if __name__ == "__main__":
+    a = 1
+    b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

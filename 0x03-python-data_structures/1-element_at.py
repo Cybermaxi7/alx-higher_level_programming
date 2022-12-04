0deadc1bd1430e0076f14c07a8f17595e6b6e130
Maximus Agbe <maximusagbe@gmail.com>
2022-12-04 13:08:32 -0800

    return's the  element of a given index value

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+""" Script return's an element based on a given index value"""
+def element_at(my_list, idx):
+    if idx < -1:
+        return None
+    if idx > len(my_list):
+        return None
+    return my_list[idx]

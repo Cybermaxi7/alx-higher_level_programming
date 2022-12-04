1b7ac8e562ced6203a35b99b3e0b8ff8ab55ee7a
Maximus Agbe <maximusagbe@gmail.com>
2022-12-04 13:21:44 -0800

    update code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/bash
-""" Script return's an element based on a given index value"""
-
-
 def element_at(my_list, idx):
     if idx <= -1:
-        return None
+        return (None)
     if idx >= len(my_list):
         return None
-    return my_list[idx]
+    return (my_list[idx])

452496d329f2bdba58527aa64cc41dd8e08d6931
Maximus Agbe <maximusagbe@gmail.com>
2022-12-04 14:02:23 -0800

    addjust pycodestyle rule

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
-        return 
+        return
     reversed_lists = reversed(my_list)
     for val in reversed_lists:
         print("{:d}".format(val))

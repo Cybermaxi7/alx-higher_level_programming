5ef2581ef40cb1636f6e744c44fbc56f444ca25b
Maximus Agbe <maximusagbe@gmail.com>
2022-12-05 04:58:41 -0800

    update code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     result = ()
     if not sentence:
         result = 0, "None"
-    else
+    else:
         result = len(sentence), sentence[0]
     return result
